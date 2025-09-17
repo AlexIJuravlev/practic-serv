d839aa511c92da5100343d3ba5f8fd329198c9dc
AlexIJuravlev <cawasimus@gmail.com>
2025-09-17 18:15:33 +0300

    cors

--- server/app.js
+++ server/app.js
@@ -10,12 +10,27 @@ const cors = require('cors')
 const port = 3001;
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://5.129.250.62",
+  "5669833-jq41137.twc1.net",
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.static('../client/dist'))
 
 app.use(cookieParser());
