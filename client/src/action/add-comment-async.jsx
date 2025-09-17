dd0eefe4f46f014661543a430aa911fe9354ab04
AlexIJuravlev <cawasimus@gmail.com>
2025-09-17 13:49:37 +0300

    add docker

--- client/src/action/add-comment-async.jsx
+++ client/src/action/add-comment-async.jsx
@@ -3,14 +3,11 @@ import { addComment } from "./add-cooment";
 
 export const addCommentAsync =
 	(postId, content) => (dispatch) => {
-		request(
-			`http://localhost:3001/posts/${postId}/comments`,
-			'POST',
-			{ content },
-		).then((comment) => {
+		request(`http://localhost:3001/posts/${postId}/comments`, 'POST', {
+			content,
+		}).then((comment) => {
 			dispatch(addComment(comment.data));
 			console.log(postId);
 			console.log(content);
-
 		});
 	};
--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use(express.json());
 
 app.use("/", routes);
 
+
 mongoose.connect(process.env.DB_CONNECTION_STRING).then(() => {
   app.listen(port, () => {
     console.log(`Server started on port ${port}`);
